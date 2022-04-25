dfb30ca2c4555db1f4ab5d96ede0817628520d19
Soufiane Essaouari <soufiane.pro@yahoo.com>
2022-04-25 12:07:18 +0200

    test_architecture-CSS-JS

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-index
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="test"><h1>Ok comme avant</h1></div>
+    <img src="image/0804.png" alt="image">
+    <script src="js/script.js"></script>
+</body>
+</html>
